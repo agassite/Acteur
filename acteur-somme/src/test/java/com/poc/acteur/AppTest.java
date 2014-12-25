d3a9a5c77271e52ba78b1a128704b8441c74dbe2
dhassoun <david.h3@free.fr>
2014-12-25 23:53:25 +0100

    mise en place tests unitaires

--- acteur-somme/src/test/java/com/poc/acteur/AppTest.java
+++ acteur-somme/src/test/java/com/poc/acteur/AppTest.java
@@ -1,5 +1,18 @@
 package com.poc.acteur;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.pattern.Patterns;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -10,6 +23,8 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+	
+	public static final int POOL_WORKER = 8;
     /**
      * Create the test case
      *
@@ -31,8 +46,29 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testApp ()
     {
-        assertTrue( true );
+    	LoggingAdapter log = null;
+    	
+    	try {
+	    	//liste d'entier a traiter
+	    	List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);  
+	    	ActorSystem system = ActorSystem.create("mySystem");  
+	    	log = Logging.getLogger(system, system);
+	    	
+	    	//creation du master
+	    	ActorRef master = system.actorOf(Props.create(Master.class,POOL_WORKER,numbers));
+	    	
+			// Creation de la future, Acteur implicite auquel notre master pourra repondre
+			Future<Object> future = Patterns
+					.ask(master, new StartMessage(), 30000);
+	    	
+			List <Integer> listeResultat = (List<Integer>) Await.result(future,
+					Duration.create(30000, TimeUnit.MILLISECONDS));
+	    	log.info("resultat= " + listeResultat);
+			system.shutdown();
+    	} catch (Exception ex) {
+    		log.error("error: " + ex.getMessage());
+    	}
     }
 }
--- acteur-supervisor/src/test/java/com/poc/acteur/AppTest.java
+++ acteur-supervisor/src/test/java/com/poc/acteur/AppTest.java
@@ -1,5 +1,21 @@
 package com.poc.acteur;
 
+import java.util.concurrent.TimeUnit;
+
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.pattern.Patterns;
+
+import com.poc.acteur.supervisor.IntegerMessage;
+import com.poc.acteur.supervisor.ResultMessageRequest;
+import com.poc.acteur.supervisor.SupervisorActor;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -33,6 +49,66 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+    	LoggingAdapter log = null;
+    	try {
+    	ActorSystem system = ActorSystem.create("faultTolerance");
+		log = Logging.getLogger(system, system);
+		
+		//creation de l'acteur superviseur
+		ActorRef supervisor = system.actorOf(
+				Props.create(SupervisorActor.class), "supervisor");
+		
+		
+		ResultMessageRequest request = new ResultMessageRequest();
+		
+		//Envoi d'un entier au superviseur
+		log.info("Envoi de  l'entier 2 ...");
+		supervisor.tell(new IntegerMessage (2),ActorRef.noSender());
+		
+		//Future pour recuperer  reponse provenant du superviseur
+		Future<Object> future = Patterns
+				.ask(supervisor, request, 5000);
+		Integer result = (Integer) Await.result(
+				future, Duration.create(5000, TimeUnit.MILLISECONDS));
+
+		//La valeur attendu est 4/2 -> 2
+		log.info("Valeur recue-> {}", result);
+		assert result.equals(Integer.valueOf(2));
+		
+		log.info("Envoi de  l'entier 0 ...");
+		supervisor.tell(new IntegerMessage (0),ActorRef.noSender());
+		
+		//Future pour recuperer  reponse provenant du superviseur
+		 future = Patterns
+				.ask(supervisor, request, 5000);
+		 result = (Integer) Await.result(
+				future, Duration.create(5000, TimeUnit.MILLISECONDS));
+
+		log.info("Valeur recue-> {}", result);
+		
+		//L'exception ArithmetiqueException a ete declenchee par le worker
+		//Le worker continue donc la valeur renvoyee est toujours 2
+		assert result.equals(Integer.valueOf(2));
+		
+		log.info("Envoi de  l'entier wrappe avec une valeur null ...");
+		supervisor.tell(new IntegerMessage (null),ActorRef.noSender());
+		
+		//Future pour recuperer  reponse provenant du superviseur
+		 future = Patterns
+				.ask(supervisor, request, 5000);
+		 result = (Integer) Await.result(
+				future, Duration.create(5000, TimeUnit.MILLISECONDS));
+
+		//L'exception NullPointerException a ete declenchee par le worker
+		//Le worker est redemaree la valeur renvoyee est la valeur initiale 4
+		log.info("Valeur recue-> {}", result);
+		assert result.equals(Integer.valueOf(4));
+
+		
+		log.info("Worker Actor shutdown !");
+		system.shutdown();
+    	} catch (Exception ex) {
+    		log.error("error: " + ex.getMessage());
+    	}
     }
 }
